00592a40cb5aaad5bf8fb1ff61167bc60eb228f2
Abdullah1dev <242677@students.au.edu.pk>
2026-07-16 20:40:34 +0500

    Add text before starting operations to make the user experince better

--- main.py
+++ main.py
@@ -10,21 +10,23 @@ response = {
 }
 
 
+
 while True:
+    print("=" * 50)
+    print("🤖 Rule-Based AI Chatbot")
+    print("=" * 50)
+    print("Hello! I'm your Rule-Based AI Chatbot.")
+    print("I can respond to a set of predefined commands.")
+    print("\nType 'help' to see the available commands.")
+    print("Type 'exit")
+    
+    
+    
     user_input = input("Enter Text : ")
     clean_input = user_input.lower().strip()
     
-    if clean_input == "exit":
+    if clean_input == "exit": 
         break
     else:
         reply = response.get(clean_input , "Invalid Entery")
         print(reply)
-   
-    
-        
-    
-
-    
-            
-        
-        
