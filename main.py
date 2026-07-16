00c04fbd101c92c89a559a88ff75fa7f06393655
Abdullah1dev <242677@students.au.edu.pk>
2026-07-16 16:26:06 +0500

    Create a dict instread of if else ladder to reduce the time complexity

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 
+response = {
+    "hello" : "hi",
+    "red" : "apple" ,
+    "yellow" : "banana",
+    "orange" : "malta",
+    "abdullah" : "tahir"
+    
+    
+}
 
 
 while True:
     user_input = input("Enter Text : ")
     clean_input = user_input.lower().strip()
+    
+    if clean_input == "exit":
+        break
+    else:
+        reply = response.get(clean_input , "Invalid Entery")
+        print(reply)
+   
+    
+        
+    
 
     
-    if clean_input == "hello":
-        print("Hi")
-    elif clean_input == "tell me about your self":
-        print("I am Abdullah Assistant")
-    elif clean_input == "what is time":
-        print("I dont know")
-        
-    elif clean_input == "exit":
-        break
-    
-    else:
-        print("Invalid Entry.")
+            
         
         
